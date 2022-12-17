aa673a626c8649527befff709715651dfc14ccc3
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-17 22:56:47 +0300

    Добавил Meta, __str__ в Category

--- advertisements/models.py
+++ advertisements/models.py
@@ -2,4 +2,12 @@ from django.db.models import Model, CharField
 
 
 class Category(Model):
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
     name = CharField(max_length=200)
