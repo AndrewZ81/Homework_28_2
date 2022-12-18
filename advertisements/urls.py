8f1fabc314958966fc8b2bf589ab76b3d3e991e1
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-18 20:52:44 +0300

    Разнес маршруты по пакетам

--- advertisements/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from advertisements import views
-
-urlpatterns = [
-    path('', views.CategoryView.as_view()),
-    path('<int:pk>/', views.CategoryDetailView.as_view()),
-]
