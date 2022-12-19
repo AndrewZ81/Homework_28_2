18babbdcc03fd34eacfb9919fc0abdc80ef59af9
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-19 10:57:05 +0300

    Создал маршруты для классов  'CategoryUpdateView', 'CategoryDeleteView'

--- advertisements/urls/categories.py
+++ advertisements/urls/categories.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.CategoryListView.as_view()),
     path('<int:pk>/', views.CategoryDetailView.as_view()),
     path('create/', views.CategoryCreateView.as_view()),
+    path('<int:pk>/update/', views.CategoryUpdateView.as_view()),
+    path('<int:pk>/delete/', views.CategoryDeleteView.as_view()),
 ]
