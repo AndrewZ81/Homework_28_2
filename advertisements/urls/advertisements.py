9e726a59547760d652f1e724630bb8bcdcf8e21d
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-19 11:10:11 +0300

    Создал маршруты для всех классов 'Advertisement...'

--- advertisements/urls/advertisements.py
+++ advertisements/urls/advertisements.py
@@ -3,6 +3,9 @@ from django.urls import path
 from advertisements import views
 
 urlpatterns = [
-    path('', views.AdvertisementView.as_view()),
+    path('', views.AdvertisementListView.as_view()),
     path('<int:pk>/', views.AdvertisementDetailView.as_view()),
+    path('create/', views.AdvertisementCreateView.as_view()),
+    path('<int:pk>/update/', views.AdvertisementUpdateView.as_view()),
+    path('<int:pk>/delete/', views.AdvertisementDeleteView.as_view()),
 ]
